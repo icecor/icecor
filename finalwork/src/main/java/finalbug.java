5bc9b1b3026b1924be978998c807fa883d695cba
icecor <a1456789sun@gmail.com>
2022-01-10 21:05:33 +0800

    抓符文

--- finalwork/src/main/java/finalbug.java
+++ finalwork/src/main/java/finalbug.java
@@ -1,5 +1,7 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class finalbug {
     public static void main(String[] args) {
@@ -8,29 +10,95 @@ public class finalbug {
             System.out.println(doc.title());
             System.out.println("上路T1角:");
 
+
             for (int a = 1; a <= 5; a++) {
                 String top = doc.select("body > div.l-wrap.l-wrap--champion > div.l-container > div.l-champion-index > div.l-champion-index-content > div.l-champion-index-content--side > div > div.champion-index-trend-content > div > div.tabItem.champion-trend.champion-trend-tier > div > table > tbody.tabItem.champion-trend-tier-TOP > tr:nth-child(" + a + ") > td.champion-index-table__cell.champion-index-table__cell--champion > a > div.champion-index-table__name").text();
                 System.out.println("排名" + a + ":" + top);
+                Elements choose = doc.select("body > div.l-wrap.l-wrap--champion > div.l-container > div.l-champion-index > div.l-champion-index-content > div.l-champion-index-content--side > div > div.champion-index-trend-content > div > div.tabItem.champion-trend.champion-trend-tier > div > table > tbody.tabItem.champion-trend-tier-TOP > tr:nth-child(" + a + ") > td.champion-index-table__cell.champion-index-table__cell--champion > a");
+                Element choose2 = choose.get(0);
+                Document doc2 = Jsoup.connect(choose2.absUrl("href")).get();
+                System.out.println(top+"最常使用的兩套符文:");
+                Elements images1 = doc2.select("tbody.tabItem.ChampionKeystoneRune-1 div.perk-page  div.perk-page__item.perk-page__item--active  div.perk-page__image >img");
+                for(int i = 0; i <= 11; i++){
+                    Element image = images1.get(i);
+                    System.out.println(image.attr("alt"));
+                    while(i == 5){
+                        System.out.println("=========");
+                        break;
+                    }
+                }
             }
             System.out.println("打野T1角");
             for (int b = 1; b <= 5; b++) {
                 String jg = doc.select("body > div.l-wrap.l-wrap--champion > div.l-container > div.l-champion-index > div.l-champion-index-content > div.l-champion-index-content--side > div > div.champion-index-trend-content > div > div.tabItem.champion-trend.champion-trend-tier > div > table > tbody.tabItem.champion-trend-tier-JUNGLE > tr:nth-child(" + b + ") > td.champion-index-table__cell.champion-index-table__cell--champion > a > div.champion-index-table__name").text();
                 System.out.println("排名" + b + ":" + jg);
+                Elements choose = doc.select("body > div.l-wrap.l-wrap--champion > div.l-container > div.l-champion-index > div.l-champion-index-content > div.l-champion-index-content--side > div > div.champion-index-trend-content > div > div.tabItem.champion-trend.champion-trend-tier > div > table > tbody.tabItem.champion-trend-tier-TOP > tr:nth-child(" + b + ") > td.champion-index-table__cell.champion-index-table__cell--champion > a");
+                Element choose2 = choose.get(0);
+                Document doc2 = Jsoup.connect(choose2.absUrl("href")).get();
+                System.out.println(jg+"最常使用的兩套符文:");
+                Elements images1 = doc2.select("tbody.tabItem.ChampionKeystoneRune-1 div.perk-page  div.perk-page__item.perk-page__item--active  div.perk-page__image >img");
+                for(int i = 0; i <= 11; i++){
+                    Element image = images1.get(i);
+                    System.out.println(image.attr("alt"));
+                    while(i == 5){
+                        System.out.println("=========");
+                        break;
+                    }
+                }
             }
             System.out.println("中路T1角");
             for (int c = 1; c <= 5; c++) {
                 String mid = doc.select("body > div.l-wrap.l-wrap--champion > div.l-container > div.l-champion-index > div.l-champion-index-content > div.l-champion-index-content--side > div > div.champion-index-trend-content > div > div.tabItem.champion-trend.champion-trend-tier > div > table > tbody.tabItem.champion-trend-tier-MID > tr:nth-child(" + c + ") > td.champion-index-table__cell.champion-index-table__cell--champion > a > div.champion-index-table__name").text();
                 System.out.println("排名 " + c + ":" + mid);
+                Elements choose = doc.select("body > div.l-wrap.l-wrap--champion > div.l-container > div.l-champion-index > div.l-champion-index-content > div.l-champion-index-content--side > div > div.champion-index-trend-content > div > div.tabItem.champion-trend.champion-trend-tier > div > table > tbody.tabItem.champion-trend-tier-TOP > tr:nth-child(" + c + ") > td.champion-index-table__cell.champion-index-table__cell--champion > a");
+                Element choose2 = choose.get(0);
+                Document doc2 = Jsoup.connect(choose2.absUrl("href")).get();
+                System.out.println(mid+"最常使用的兩套符文:");
+                Elements images1 = doc2.select("tbody.tabItem.ChampionKeystoneRune-1 div.perk-page  div.perk-page__item.perk-page__item--active  div.perk-page__image >img");
+                for(int i = 0; i <= 11; i++){
+                    Element image = images1.get(i);
+                    System.out.println(image.attr("alt"));
+                    while(i == 5){
+                        System.out.println("=========");
+                        break;
+                    }
+                }
             }
             System.out.println("下路T1角");
             for (int d = 1; d <= 2; d++) {
                 String ad = doc.select("body > div.l-wrap.l-wrap--champion > div.l-container > div.l-champion-index > div.l-champion-index-content > div.l-champion-index-content--side > div > div.champion-index-trend-content > div > div.tabItem.champion-trend.champion-trend-tier > div > table > tbody.tabItem.champion-trend-tier-ADC > tr:nth-child(" + d + ") > td.champion-index-table__cell.champion-index-table__cell--champion > a > div.champion-index-table__name").text();
                 System.out.println("排名" + d + ":" + ad);
+                Elements choose = doc.select("body > div.l-wrap.l-wrap--champion > div.l-container > div.l-champion-index > div.l-champion-index-content > div.l-champion-index-content--side > div > div.champion-index-trend-content > div > div.tabItem.champion-trend.champion-trend-tier > div > table > tbody.tabItem.champion-trend-tier-TOP > tr:nth-child(" + d + ") > td.champion-index-table__cell.champion-index-table__cell--champion > a");
+                Element choose2 = choose.get(0);
+                Document doc2 = Jsoup.connect(choose2.absUrl("href")).get();
+                System.out.println(ad+"最常使用的兩套符文:");
+                Elements images1 = doc2.select("tbody.tabItem.ChampionKeystoneRune-1 div.perk-page  div.perk-page__item.perk-page__item--active  div.perk-page__image >img");
+                for(int i = 0; i <= 11; i++){
+                    Element image = images1.get(i);
+                    System.out.println(image.attr("alt"));
+                    while(i == 5){
+                        System.out.println("=========");
+                        break;
+                    }
+                }
             }
             System.out.println("輔助T1角");
             for (int e = 1; e <= 4; e++) {
                 String sup = doc.select("body > div.l-wrap.l-wrap--champion > div.l-container > div.l-champion-index > div.l-champion-index-content > div.l-champion-index-content--side > div > div.champion-index-trend-content > div > div.tabItem.champion-trend.champion-trend-tier > div > table > tbody.tabItem.champion-trend-tier-SUPPORT > tr:nth-child(" + e + ") > td.champion-index-table__cell.champion-index-table__cell--champion > a > div.champion-index-table__name").text();
                 System.out.println("排名" + e + ":" + sup);
+                Elements choose = doc.select("body > div.l-wrap.l-wrap--champion > div.l-container > div.l-champion-index > div.l-champion-index-content > div.l-champion-index-content--side > div > div.champion-index-trend-content > div > div.tabItem.champion-trend.champion-trend-tier > div > table > tbody.tabItem.champion-trend-tier-TOP > tr:nth-child(" + e + ") > td.champion-index-table__cell.champion-index-table__cell--champion > a");
+                Element choose2 = choose.get(0);
+                Document doc2 = Jsoup.connect(choose2.absUrl("href")).get();
+                System.out.println(sup+"最常使用的兩套符文:");
+                Elements images1 = doc2.select("tbody.tabItem.ChampionKeystoneRune-1 div.perk-page  div.perk-page__item.perk-page__item--active  div.perk-page__image >img");
+                for(int i = 0; i <= 11; i++){
+                    Element image = images1.get(i);
+                    System.out.println(image.attr("alt"));
+                    while(i == 5){
+                        System.out.println("=========");
+                        break;
+                    }
+                }
             }
 
         } catch (Exception e) {
